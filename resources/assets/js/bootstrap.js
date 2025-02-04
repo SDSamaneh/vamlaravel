ff28b0fc7ceee86cdee2159af1abd98969650a61
SDSamaneh <samame.sedigh@gmail.com>
2025-02-04 13:56:47 +0330

    base

---
+++ resources/assets/js/bootstrap.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import '../vendor/bootstrap/dist/js/bootstrap.bundle.min.js';
+import './functions.js';
+
+import.meta.glob(['../images/**']);
+
+
+
+
